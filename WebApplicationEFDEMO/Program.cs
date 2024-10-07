5d59aeaebab1ef982565e115a4c33850b95631c9
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2024-10-07 14:03:19 +0200

    set sql in program.cs and added migrations

--- WebApplicationEFDEMO/Program.cs
+++ WebApplicationEFDEMO/Program.cs
@@ -1,3 +1,7 @@
+using DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 namespace WebApplicationEFDEMO
 {
     public class Program
@@ -10,14 +14,20 @@ namespace WebApplicationEFDEMO
 
             builder.Services.AddControllers();
 
+            builder.Services.AddDbContext<CustomerContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+            });
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            app.UseRouting();
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
