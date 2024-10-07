8a4f2602a10a878a88d12ab255f20e1da4ce1ebd
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2024-10-07 12:27:27 +0200

    added DbContext

--- DAL/CustomerContext.cs
+++ DAL/CustomerContext.cs
@@ -1,7 +1,20 @@
-﻿namespace DAL
-{
-    public class Class1
-    {
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 
+namespace DAL
+{
+    public class CustomerContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+        public DbSet<InvoiceLine> InvoicesLine { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<EmployeeRole> EmployeeRole { get; set; }
+
+        public CustomerContext()
+        {
+
+        }
     }
+
 }
